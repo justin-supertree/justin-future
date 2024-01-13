ea7fd28991cc02b71332de69419c378b7d0c45de
justin-supertree <justin1688@supertree.co>
2024-01-13 14:58:34 +0900

    feat: footer 제거

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -37,7 +37,7 @@ const Layout = ({ children }: Props) => {
         <MainSection>
           <Header />
           <MainBlock>{children}</MainBlock>
-          <Footer />
+          {/* <Footer /> */}
         </MainSection>
       </Container>
     </>
