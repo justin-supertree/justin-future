2788c3f38af0098ddf4e115bd58897d4d51d373a
justin-supertree <justin1688@supertree.co>
2024-01-14 19:33:36 +0900

    feat: header 중앙 코드 수정

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -3,6 +3,7 @@ import styled from "@emotion/styled";
 const Container = styled.div`
   display: flex;
   align-items: center;
+  justify-content: center;
   width: 100%;
   padding: 2rem 3rem;
   background-color: lightcoral;
@@ -15,8 +16,6 @@ const Container = styled.div`
 const InnerWrapper = styled.div`
   width: 100%;
   max-width: 768px;
-  margin: 0;
-  text-align: center;
 `;
 
 const Title = styled.h1`
